124e383699fe5d10898e4e6faaedaca3b11f2d8c
Administrator <Administrator@192.168.123.100>
2017-02-05 23:21:04 +0900

    0.1.4

--- deep/src/com/deep/model/domain/Follow.java
+++ deep/src/com/deep/model/domain/Follow.java
@@ -1,5 +1,38 @@
 package com.deep.model.domain;
 
 public class Follow {
+	private int deepFollowing;
+	private int deepFollower;
+	
+	public Follow() {
+		super();
+	}
+	
+	public int getdeepFollowing()
+	{
+		return deepFollowing;
+	}
+	
+	public void setdeepFollowing(int deepFollowing)
+	{
+		this.deepFollowing = deepFollowing;
+	}
+	
+	public int getdeepFollower()
+	{
+		return deepFollower;
+	}
+	
+	public void setdeepFollower(int deepFollower)
+	{
+		this.deepFollower = deepFollower;
+	}
+	
+	public Follow(int deepFollowing, int deepFollower)
+	{
+		super();
+		this.deepFollowing = deepFollowing;
+		this.deepFollower = deepFollower;
+	}
 
 }
