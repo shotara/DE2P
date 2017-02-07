aa321e030e06857b89d7a11e4d3a308a3e25ae74
Administrator <Administrator@192.168.123.100>
2017-02-08 02:44:12 +0900

    0.3.1

--- deep/src/com/deep/model/FollowDAO.java
+++ deep/src/com/deep/model/FollowDAO.java
@@ -1,5 +1,76 @@
 package com.deep.model;
 
-public class FollowDAO {
+import org.apache.ibatis.session.SqlSession;
 
+import com.deep.model.domain.Follow;
+import com.deep.util.DAOFactory;
+
+
+public class FollowDAO {
+	private static final String namespace = "Follow";
+	
+	public static int getFollower(int deepFollower){ // 로그인했다는 전제니까. 매개변수x?
+		SqlSession sqlSession = DAOFactory.getSqlSession(true);
+		
+		try
+		{
+			return (int)sqlSession.selectOne(namespace +" .getFollower", deepFollower);
+		}
+			finally {
+				sqlSession.close();
+			}
+	}
+	
+	public static int getFollowing(int deepFollower) // 팔로워에 따른 팔로잉
+	{
+		SqlSession sqlSession = DAOFactory.getSqlSession(true);
+		try
+		{
+			return (int)sqlSession.selectOne(namespace +" .getFollowing", deepFollower);
+			//set List??
+		}
+		finally{
+			sqlSession.close();
+		}
+	}
+	
+	public static int addFollow(int deepFollowing)
+	{
+		SqlSession sqlSession = DAOFactory.getSqlSession(false);
+		try
+		{
+			int addFo = (int)sqlSession.insert(namespace + " .addFollow", deepFollowing);
+			if(addFo == 1) { //2명한꺼번에 add가능한가...?
+				sqlSession.commit();
+				return addFo;
+			} else {
+				sqlSession.rollback();
+				return addFo;
+			}
+		}
+		finally{
+				sqlSession.close();
+		}
+	}
+	
+	public static int setFollow(int deepFollowing)
+	{
+		SqlSession sqlSession = DAOFactory.getSqlSession(false);
+		try
+		{
+			int setFo = (int)sqlSession.delete(namespace + " .setFollow", deepFollowing);
+			if(setFo == 1){ //여러명 삭제할수 있나?
+				sqlSession.commit();
+				return setFo;
+			} else{
+				sqlSession.rollback();
+				return setFo;
+			}
+		}
+		finally{
+			sqlSession.close();
+		}
+	}
+	
+	
 }
--- deep/src/com/deep/model/domain/Follow.java
+++ deep/src/com/deep/model/domain/Follow.java
@@ -4,29 +4,39 @@ public class Follow {
 	private int deepFollowing;
 	private int deepFollower;
 	
-	public Follow() {
-		super();
-	}
-	
-	public int getdeepFollowing() {
+	public int getDeepFollowing() {
 		return deepFollowing;
 	}
-	
-	public void setdeepFollowing(int deepFollowing)	{
+
+
+
+	public void setDeepFollowing(int deepFollowing) {
 		this.deepFollowing = deepFollowing;
 	}
-	
-	public int getdeepFollower() {
+
+
+
+	public int getDeepFollower() {
 		return deepFollower;
 	}
-	
-	public void setdeepFollower(int deepFollower) {
+
+
+
+	public void setDeepFollower(int deepFollower) {
 		this.deepFollower = deepFollower;
 	}
-	
+
+	public Follow() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
 	public Follow(int deepFollowing, int deepFollower) {
 		super();
 		this.deepFollowing = deepFollowing;
 		this.deepFollower = deepFollower;
 	}
+
 }
